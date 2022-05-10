e217f78e99399007b6da0a7d6bdc05f7f8fe934a
Dev <abdullah.badar@devsinc.vom>
2022-05-10 17:51:00 +0500

    added production url

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,6 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
-
+  config.action_mailer.default_url_options = { host: 'https://boiling-lowlands-20810.herokuapp.com' }
   # Code is not reloaded between requests.
   config.cache_classes = true
 
