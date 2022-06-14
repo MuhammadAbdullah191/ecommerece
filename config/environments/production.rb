9f0dd80ce9c9d2ae17ca274350ffeb1dddd71589
Dev <abdullah.badar@devsinc.vom>
2022-06-14 14:03:20 +0500

    send mails

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -10,7 +10,7 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
     user_name: ENV['SENDMAIL_USERNAME'],
     password: ENV['SENDMAIL_PASSWORD'],
-    domain: 'https://boiling-lowlands-20810.herokuapp.com',
+    domain: 'gmail.com',
     address: 'smtp.gmail.com',
     port: '587',
     authentication: :plain,
