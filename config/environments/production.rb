4fea58bf794ab2ada0689dc240e08df0e08a7f79
Dev <abdullah.badar@devsinc.vom>
2022-06-14 13:46:58 +0500

    send mails

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -6,7 +6,7 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.default_url_options = { host: 'https://boiling-lowlands-20810.herokuapp.com' }
   # Code is not reloaded between requests.
-  config.cache_classes = true
+
   config.action_mailer.smtp_settings = {
     user_name: ENV['SENDMAIL_USERNAME'],
     password: ENV['SENDMAIL_PASSWORD'],
@@ -16,6 +16,8 @@ Rails.application.configure do
     authentication: :plain,
     enable_starttls_auto: true
   }
+
+  config.cache_classes = true
   # Eager load code on boot. This eager loads most of Rails and
   # your application in memory, allowing both threaded web servers
   # and those relying on copy on write to perform better.
